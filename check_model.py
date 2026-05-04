5c9715a2c52215cc629cfe70a488bb9436525faf
Huong <thuyhuonga3s@gmail.com>
2026-05-04 17:56:56 +0700

    Fix NameError short_text

--- check_model.py
+++ check_model.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAapV_7fajRmM7RZWjZaFM_UcDHlgFQ-O4")
+genai.configure(api_key="AIzaSyCN6MtOlNhoat9tUNQ96hqtLPVpBNultHs")
 
 print("Danh sách các model bạn được phép dùng:")
 for m in genai.list_models():
